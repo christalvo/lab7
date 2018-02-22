9ac65b7a9451dd6c80c5ff651456dad0ebf95897
christalvo <christalvo@gmail.com>
2018-02-22 10:32:53 -0800

    Added listener on Like buttons

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,13 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+
+}
+
+$('button').click(likeClick);
+
+function likeClick(event) {
+	console.log("Clicked on Like");
+	event.preventDefault();
+	ga("send", "event", 'like', 'click');
 }
